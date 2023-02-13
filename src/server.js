a4294162dcc057360fd1525b757b269d55d506ec
Guilherme Ramos <guilherme.silvano@hotmail.com>
2023-02-13 18:47:13 -0300

    refecthing

--- src/server.js
+++ src/server.js
@@ -20,9 +20,25 @@ app.get('/saved', async (req, res) => {
 
 app.get('/', async (req, res) => {
     const productsFromCache = await client.get('getAllProducts')
+    const isProductsFromCacheStale = !(await client.get('getAllProducts:validation'))
+
+    if (isProductsFromCacheStale) {
+        const isRefetching = !!(await client.get('getAllProducts:is-refetching'))
+        console.log({ isRefetching })
+        if (!isRefetching) {
+            await client.set('getAllProducts:is-refetching', 'true', { EX: 20 })
+            setTimeout(async () => {
+                console.log('cache is stale, refetching...')
+                const products = await getAllProducts()
+                await client.set('getAllProducts', JSON.stringify(products))
+                await client.set('getAllProducts:validation', 'true', { EX: 5 })
+            }, 0)
+        }
+    }
     if (productsFromCache) {
         return res.send(JSON.parse(productsFromCache))
     }
+
     const products = await getAllProducts();
     //Expiração
     // await client.set('getAllProducts', JSON.stringify(products), { EX: 10 })
